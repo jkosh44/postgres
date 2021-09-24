852ad2bdf215fcb0b0c441c42592605d6d093b24
Joseph Koshakow <koshy44@gmail.com>
2021-09-24 11:59:36 -0400

    cow

--- cmudb/exploration/pgnp_docker.py
+++ cmudb/exploration/pgnp_docker.py
@@ -39,6 +39,7 @@ def start_exploration_docker() -> subprocess.Popen:
     # Hack to wait for container to start
     time.sleep(5)
     print("LOOK HERE FOR THE DEVIL")
+    print(f"Would terminate {'Exploring' in compose.stdout}")
     print(compose.stdout)
     return compose
 
