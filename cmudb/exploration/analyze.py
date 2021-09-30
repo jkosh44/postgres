08342bfcc0129878da812047296dbe2b0d45145a
Joseph Koshakow <koshy44@gmail.com>
2021-09-30 11:57:55 -0400

    Add summary statistics

--- cmudb/exploration/analyze.py
+++ cmudb/exploration/analyze.py
@@ -11,16 +11,16 @@ VALID = "valid"
 NS_PER_SEC = 1000000000
 
 # TODO turn into command line arg
-FILE_NAME = "results/not_cow_tuned_12hr.json"
+FILE_NAME = "results/not_cow_tuned_12hr_checkpoint.json"
 
 
 def main():
     df = pd.read_json(FILE_NAME)
     df[COPY_TIME] = df[COPY_TIME_NS] / NS_PER_SEC
     df[STARTUP_TIME] = df[STARTUP_TIME_NS] / NS_PER_SEC
-    df.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME], kind="line", title="Not COW (seconds)")
-    plt.show()
-    # print(df)
+    df.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME], kind="line", title="COW with Checkpoints (seconds)")
+    # plt.show()
+    print(df[[STARTUP_TIME, COPY_TIME]].describe())
 
 
 if __name__ == '__main__':
