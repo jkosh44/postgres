b0d8026658a3160a7447342ba8a4d2e3cb87160a
Joseph Koshakow <koshy44@gmail.com>
2021-09-30 21:40:06 -0400

    Add some stats

--- cmudb/exploration/analyze.py
+++ cmudb/exploration/analyze.py
@@ -11,16 +11,19 @@ VALID = "valid"
 NS_PER_SEC = 1000000000
 
 # TODO turn into command line arg
-FILE_NAME = "results/not_cow_tuned_12hr_checkpoint.json"
+FILE_NAME = "results/cow_tuned_12hr.json"
 
 
 def main():
     df = pd.read_json(FILE_NAME)
     df[COPY_TIME] = df[COPY_TIME_NS] / NS_PER_SEC
     df[STARTUP_TIME] = df[STARTUP_TIME_NS] / NS_PER_SEC
-    df.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME], kind="line", title="COW with Checkpoints (seconds)")
-    # plt.show()
+    valid_measurements = df[df[VALID] == True]
+    valid_measurements.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME],
+                            kind="line", title="COW (seconds)")
+    plt.show()
     print(df[[STARTUP_TIME, COPY_TIME]].describe())
+    print(f"Percent Valid: {len(valid_measurements) / len(df)}")
 
 
 if __name__ == '__main__':
