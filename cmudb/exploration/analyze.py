b2abe5150797a1838708cb6d96dd7f9aeabc3cf0
Joseph Koshakow <koshy44@gmail.com>
2021-10-18 10:44:19 -0400

    Add new stats

--- cmudb/exploration/analyze.py
+++ cmudb/exploration/analyze.py
@@ -2,29 +2,49 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 ITERATION = "iteration"
+CHECKPOINT_TIME_NS = "checkpoint_time_ns"
+CHECKPOINT_TIME = "checkpoint_time"
 COPY_TIME_NS = "copy_time_ns"
 COPY_TIME = "copy_time"
 STARTUP_TIME_NS = "startup_time_ns"
 STARTUP_TIME = "startup_time"
+TEARDOWN_TIME_NS = "teardown_time_ns"
+TEARDOWN_TIME = "teardown_time"
+TOTAL_TIME = "total_time"
 VALID = "valid"
 
 NS_PER_SEC = 1000000000
 
 # TODO turn into command line arg
-FILE_NAME = "results/experiment_1633388549.1526945.json"
+FILE_NAME = "results/zfs/cow_tuned_12_checkpoint_detailed.json"
 
 
 def main():
+    measurements = [
+        (CHECKPOINT_TIME_NS, CHECKPOINT_TIME),
+        (COPY_TIME_NS, COPY_TIME),
+        (STARTUP_TIME_NS, STARTUP_TIME),
+        (TEARDOWN_TIME_NS, TEARDOWN_TIME)
+    ]
+    sec_measurements = [sec_measurement for _, sec_measurement in measurements]
+
     df = pd.read_json(FILE_NAME)
-    df[COPY_TIME] = df[COPY_TIME_NS] / NS_PER_SEC
-    df[STARTUP_TIME] = df[STARTUP_TIME_NS] / NS_PER_SEC
+    df[TOTAL_TIME] = 0
+    for ns_measurement, sec_measurement in measurements:
+        df[sec_measurement] = df[ns_measurement] / NS_PER_SEC
+        df[TOTAL_TIME] += df[sec_measurement]
+
+
     valid_measurements = df[df[VALID]]
-    # valid_measurements.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME],
+    # valid_measurements.plot(x=ITERATION, y=sec_measurements,
     #                         kind="line", title="COW (seconds)")
-    valid_measurements.plot(x=ITERATION, y=[COPY_TIME, STARTUP_TIME],
-                            kind="bar", stacked=True, title="COW (seconds)")
+    valid_measurements.plot(x=ITERATION, y=sec_measurements, kind="bar",
+                            stacked=True, title="COW (seconds)")
     plt.show()
-    print(df[[STARTUP_TIME, COPY_TIME]].describe())
+
+    for sec_measurement in sec_measurements:
+        print(valid_measurements[sec_measurement].describe())
+    print(valid_measurements[TOTAL_TIME].describe())
     print(f"Percent Valid: {len(valid_measurements) / len(df)}")
 
 
