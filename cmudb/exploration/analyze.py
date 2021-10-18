3e6c93caaff4b5111cbe188ec473cf9b6ae79e4c
Joseph Koshakow <koshy44@gmail.com>
2021-10-18 12:45:12 -0400

    COW

--- cmudb/exploration/analyze.py
+++ cmudb/exploration/analyze.py
@@ -6,8 +6,10 @@ CHECKPOINT_TIME_NS = "checkpoint_time_ns"
 CHECKPOINT_TIME = "checkpoint_time"
 COPY_TIME_NS = "copy_time_ns"
 COPY_TIME = "copy_time"
-STARTUP_TIME_NS = "startup_time_ns"
-STARTUP_TIME = "startup_time"
+DOCKER_STARTUP_TIME_NS = "docker_startup_time_ns"
+DOCKER_STARTUP_TIME = "docker_startup_time"
+POSTGRES_STARTUP_TIME_NS = "postgres_startup_time_ns"
+POSTGRES_STARTUP_TIME = "postgre_startup_time"
 TEARDOWN_TIME_NS = "teardown_time_ns"
 TEARDOWN_TIME = "teardown_time"
 TOTAL_TIME = "total_time"
@@ -16,14 +18,15 @@ VALID = "valid"
 NS_PER_SEC = 1000000000
 
 # TODO turn into command line arg
-FILE_NAME = "results/zfs/cow_tuned_12_checkpoint_detailed.json"
+FILE_NAME = "results/zfs/cow_tuned_12hr_checkpoint_multidisk_short.json"
 
 
 def main():
     measurements = [
         (CHECKPOINT_TIME_NS, CHECKPOINT_TIME),
         (COPY_TIME_NS, COPY_TIME),
-        (STARTUP_TIME_NS, STARTUP_TIME),
+        (DOCKER_STARTUP_TIME_NS, DOCKER_STARTUP_TIME),
+        (POSTGRES_STARTUP_TIME_NS, POSTGRES_STARTUP_TIME),
         (TEARDOWN_TIME_NS, TEARDOWN_TIME)
     ]
     sec_measurements = [sec_measurement for _, sec_measurement in measurements]
