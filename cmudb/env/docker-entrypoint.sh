d1934a9dee2d6f9f8c7e506b47007bb75dbc049b
Joseph Koshakow <koshy44@gmail.com>
2021-10-26 14:08:21 -0400

    Remove chown

--- cmudb/env/docker-entrypoint.sh
+++ cmudb/env/docker-entrypoint.sh
@@ -241,6 +241,7 @@ _main_replica() {
 
 _main_exploratory() {
   sudo chmod 700 "${PGDATA}"
+  sudo chown -R terrier:terrier "${PGDATA}"
   rm -f "${PGDATA}/postmaster.pid"
   rm -f "${PGDATA}/standby.signal"
   "${BIN_DIR}"/pg_resetwal -f "${PGDATA}"
