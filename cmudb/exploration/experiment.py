db10ef51ca45c985bab25e02f26b03fceb1effa5
Joseph Koshakow <koshy44@gmail.com>
2021-11-11 20:58:49 -0500

    COW

--- cmudb/exploration/experiment.py
+++ cmudb/exploration/experiment.py
@@ -198,15 +198,15 @@ def main():
     pg_stat_replica_thread.start()
     pg_stat_primary_thread.start()
 
-    # Block until warmup is done
-    print("Block until warmup is over")
-    for line in iter(lambda: benchbase_proc.stdout.readline(), b''):
-        if isinstance(line, bytes):
-            line = line.decode(UTF_8)
-        print(line, end="")
-        if "starting measurements" in line:
-            break
-    print("Blocking done")
+    # # Block until warmup is done
+    # print("Block until warmup is over")
+    # for line in iter(lambda: benchbase_proc.stdout.readline(), b''):
+    #     if isinstance(line, bytes):
+    #         line = line.decode(UTF_8)
+    #     print(line, end="")
+    #     if "starting measurements" in line:
+    #         break
+    # print("Blocking done")
 
     collect_results(benchbase_proc, result_file)
 
