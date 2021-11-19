e9e94afd03c381aa015661f97c66d3cd6e8d8df3
Joseph Koshakow <koshy44@gmail.com>
2021-11-19 08:43:02 -0500

    COW

--- cmudb/exploration/lag_analyze.py
+++ cmudb/exploration/lag_analyze.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-TIME = "time"
+RAW_TIME = "time"
 REPLICA_LAG = "replica_lag"
+CLEAN_TIME = "clean_time"
 
 NS_PER_SEC = 1000000000
 
@@ -12,11 +13,23 @@ EXPLORATORY_FILE_NAME = "results/final/zfs/replay_lag/exploratory_time_163728278
 
 
 def main():
-    exploratory_startup = 0
-    with open(EXPLORATORY_FILE_NAME, "")
     df = pd.read_json(LAG_FILE_NAME)
 
-    df.plot(x=TIME, y=REPLICA_LAG, kind="line", title="Replay lag")
+    start_time = df[RAW_TIME][0]
+    print(start_time)
+
+    with open(EXPLORATORY_FILE_NAME, "r") as f:
+        exploratory_startup = (int(f.readline()) - start_time) / NS_PER_SEC
+
+    print(exploratory_startup)
+
+    df[CLEAN_TIME] = (df[RAW_TIME] - start_time) / NS_PER_SEC
+
+    df = df[df[CLEAN_TIME] < 2692]
+
+    df.plot(x=CLEAN_TIME, y=REPLICA_LAG, kind="line", title="Replay lag", legend=None, xlabel="Time (sec)",
+            ylabel="Lag (sec)")
+    plt.axvline(x=exploratory_startup)
     plt.show()
 
 
